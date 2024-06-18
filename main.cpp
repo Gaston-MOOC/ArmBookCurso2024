080ce98ebfe8f5ab9df34b33acd2ea3fe8a94580
Gaston-MOOC <gmarquez.miriadax@gmail.com>
2024-06-18 21:16:29 +0000

    cambio a LED2

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ int main()
 {
     DigitalIn B1_USER(BUTTON1);
 
-    DigitalOut LD1(LED1);
+    DigitalOut Led(LED2);
 
     while (true) {
-        LD1 = B1_USER;
+        Led = ! B1_USER;
     }
 }
