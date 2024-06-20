639dc33d50e07a4256213b6af4be56b6fbdb0a5a
Gaston-MOOC <gmarquez.miriadax@gmail.com>
2024-06-20 19:48:58 +0000

    Added Led Red

--- main.cpp
+++ main.cpp
@@ -5,8 +5,10 @@ int main()
     DigitalIn B1_USER(BUTTON1);
 
     DigitalOut Led(LED2);
+    DigitalOut LedRed(LED3);
 
     while (true) {
         Led = ! B1_USER;
+        LedRed = ! B1_USER;
     }
 }
